6caae09e3a24a9d179f1d9ee3bd7c9d6c82c485e
Wu Tianji <wtj729651395@163.com>
2019-08-30 22:20:49 +0800

    Create 题5-替换空格.py

---
+++ 题5-替换空格.py
@@ -0,0 +1,26 @@
+def respace(s):
+    count = 0
+    for i in s:
+        if i == ' ':
+            count += 1
+    if count == 0:
+        return s
+    newlen = len(s) + count*2
+    newstr = [None] * newlen
+    index = len(s) -1
+    jn = newlen - 1
+    while index >= 0 :
+        if s[index] == " ":
+            newstr[jn-2:jn+1] = ['%','2','0']
+            index -= 1
+            jn -= 3
+        else:
+            newstr[jn] = s[index]
+            index -= 1
+            jn -= 1
+        print(newstr)
+    return ''.join(newstr)
+
+
+s = 'we are happy.'
+print(respace(s))
