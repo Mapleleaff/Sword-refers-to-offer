fead06460426ae8f6a68762dbf9f6ebde43b1cb5
Wu Tianji <wtj729651395@163.com>
2019-09-02 21:19:34 +0800

    Create 题9-用两个栈实现队列.py

---
+++ 题9-用两个栈实现队列.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+
+    def push(self, x):
+        self.stack1.append(x)
+
+    def pop(self):
+        if self.stack2 != []:
+            return self.stack2.pop()
+
+        while self.stack1 != []:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+
+P = Solution()
+P.push(10)
+P.push(11)
+P.push(12)
+print(P.pop())
+P.push(13)
+print(P.pop())
+print(P.pop())
+print(P.pop())
