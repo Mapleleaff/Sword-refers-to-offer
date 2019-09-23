27391865bb80531369de53a89871884f4bd6b55a
Wu Tianji <wtj729651395@163.com>
2019-09-23 21:08:36 +0800

    Update 题4-二维数组中的查找.py

--- 题4-二维数组中的查找.py
+++ 题4-二维数组中的查找.py
@@ -1,20 +1,23 @@
+"""
+题4-二维数组中的查找
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+"""
+
 def findnum(mat, num):
     if mat == None:
         return False
-
+    
     n = len(mat)-1
     m = len(mat[0])-1
     i = 0
     while (i <= m) and (n >= 0):
         if mat[n][i] < num:
             i += 1
-
         elif mat[n][i] > num:
             n -= 1
-
         else:
             return True
-
     return False
 
 
