3130ccd942fc6c291f719355e24518e35c17c32b
Wu Tianji <wtj729651395@163.com>
2019-09-23 20:11:58 +0800

    Create 题18.2-删除链表中重复的节点.py

---
+++ 题18.2-删除链表中重复的节点.py
@@ -0,0 +1,52 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteDuplication(self, pHead):
+        if pHead == None:
+            return pHead
+        preNode, curNode = None, pHead
+        nextNode = curNode.next
+
+        while nextNode != None:
+            while nextNode.val != curNode.val:
+                preNode = curNode
+                curNode = nextNode
+                nextNode = nextNode.next
+
+            while nextNode.val == curNode.val:
+                nextNode = nextNode.next
+
+            if preNode == None:
+                pHead = nextNode
+                curNode = nextNode
+                continue
+
+            else:
+                preNode.next = nextNode
+                curNode = nextNode
+                nextNode = curNode.next
+
+        return pHead
+
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(3)
+node5 = ListNode(4)
+node6 = ListNode(4)
+node7 = ListNode(5)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+
+s = Solution()
+print(s.deleteDuplication(node1).next.val)
+
+
