c7fded60c110c135ed00b2b6c491148b86c60339
Wu Tianji <wtj729651395@163.com>
2019-09-23 21:35:03 +0800

    Update 题21-调整数组顺序使奇数位于偶数前面.py

--- 题21-调整数组顺序使奇数位于偶数前面.py
+++ 题21-调整数组顺序使奇数位于偶数前面.py
@@ -1,3 +1,9 @@
+"""
+题21-调整数组顺序使奇数位于偶数前面
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分
+"""
+
 class Solution(object):
     def orderArray(self, array):
         for i in range(len(array)-1, -1, -1):
