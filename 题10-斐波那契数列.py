30916d89d7e974f449aab0df804b3ac4da8f262b
Wu Tianji <wtj729651395@163.com>
2019-09-24 23:07:24 +0800

    Update 题10-斐波那契数列.py

--- 题10-斐波那契数列.py
+++ 题10-斐波那契数列.py
@@ -1,11 +1,16 @@
+"""
+题10-斐波那契数列
+大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）
+"""
+
 # 循环解法
 def Fibonacci(n):
     N0 = 0
     N1 = 1
-
+    
     if n<=1:
         return n
-
+    
     while n>1:
         N1 = N0 + N1
         N0 = N1 - N0
@@ -17,7 +22,6 @@ def Fibonacci(n):
 def Fibonaccii(n):
     if n<=1:
         return n
-
     return Fibonaccii(n-1) + Fibonaccii(n-2)
 
 
