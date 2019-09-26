12cc6f0eac27a16e0537dc7b5690812c61b61761
Wu Tianji <wtj729651395@163.com>
2019-09-26 19:14:39 +0800

    Create 题26-树的子结构.py

---
+++ 题26-树的子结构.py
@@ -0,0 +1,59 @@
+"""
+题26-树的子结构
+输入两棵二叉树 A，B，判断 B 是不是 A 的子结构
+空树不是任意一个树的子结构
+"""
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def HasSubtree(self, pRoot1, pRoot2):
+        result = False
+        if pRoot1 != None and pRoot2 != None:
+            if pRoot1.val == pRoot2.val:
+                result = self.DoesTree1haveTree2(pRoot1, pRoot2)
+            if not result:
+                result = self.HasSubtree(pRoot1.left, pRoot2)
+            if not result:
+                result = self.HasSubtree(pRoot1.right, pRoot2)
+        return result
+
+    # 用于递归判断树的每个节点是否相同
+    # 需要注意的地方是: 前两个 if 语句不可以颠倒顺序
+    # 如果颠倒顺序, 会先判断 pRoot1 是否为 None, 其实这个时候 pRoot2 的结点已经遍历完成确定相等了, 但是返回了False, 判断错误
+    def DoesTree1haveTree2(self, pRoot1, pRoot2):
+        if pRoot2 == None:
+            return True
+        if pRoot1 == None:
+            return False
+        if pRoot1.val != pRoot2.val:
+            return False
+
+        return self.DoesTree1haveTree2(pRoot1.left, pRoot2.left) and self.DoesTree1haveTree2(pRoot1.right, pRoot2.right)
+
+pRoot1 = TreeNode(8)
+pRoot2 = TreeNode(8)
+pRoot3 = TreeNode(7)
+pRoot4 = TreeNode(9)
+pRoot5 = TreeNode(2)
+pRoot6 = TreeNode(4)
+pRoot7 = TreeNode(7)
+pRoot1.left = pRoot2
+pRoot1.right = pRoot3
+pRoot2.left = pRoot4
+pRoot2.right = pRoot5
+pRoot5.left = pRoot6
+pRoot5.right = pRoot7
+
+pRoot8 = TreeNode(8)
+pRoot9 = TreeNode(9)
+pRoot10 = TreeNode(2)
+pRoot8.left = pRoot9
+pRoot8.right = pRoot10
+
+S = Solution()
+print(S.HasSubtree(pRoot1, pRoot8))
