b8b01aef4a25256850062736d82ba4617ae8d71c
Wu Tianji <wtj729651395@163.com>
2019-09-26 19:22:14 +0800

    Update 题14-剪绳子.py

--- 题14-剪绳子.py
+++ 题14-剪绳子.py
@@ -1,44 +1,51 @@
-# 动态规划
-def Maxproduct(num):
+"""
+题14-剪绳子
+把一根长度为 n 的绳子剪成 m 段，并且使得每段的长度的乘积最大（n, m 均为整数）
+"""
 
-    # 因为至少剪一次
-    if num < 2:
-        return 0
-    if num == 2:
-        return 1
-    if num == 3:
-        return 2
-    product = [0,1,2,3]
-    for i in range(4, num+1):
-        max_dot = 0
-        for j in range(1,i//2+1):
-            pro = product[j] * product[i-j]
-            if max_dot < pro:
-                max_dot = pro
-        product.append(max_dot)
+class Solution(object):
+    # 动态规划
+    def Maxproduct(self,num):
 
-    max_dot = product[-1]
-    return max_dot
+        # 因为至少剪一次
+        if num < 2:
+            return 0
+        if num == 2:
+            return 1
+        if num == 3:
+            return 2
+        product = [0,1,2,3]
+        for i in range(4, num+1):
+            max_dot = 0
+            for j in range(1,i//2+1):
+                pro = product[j] * product[i-j]
+                if max_dot < pro:
+                    max_dot = pro
+            product.append(max_dot)
 
-
-# 贪心法算法
-def maxproduct(num):
-    # 因为至少剪一次
-    if num < 2:
-        return 0
-    if num == 2:
-        return 1
-    if num == 3:
-        return 2
-
-    n3 = i//3
-    if i%3 == 1:
-        n3 -=1
-    n2 = (num - 3*n3)//2
-
-    return pow(3,n3)*pow(2,n2)
+        max_dot = product[-1]
+        return max_dot
 
 
+    # 贪心法算法
+    def maxproduct(self,num):
+        
+        # 因为至少剪一次
+        if num < 2:
+            return 0
+        if num == 2:
+            return 1
+        if num == 3:
+            return 2
+
+        n3 = i//3
+        if i%3 == 1:
+            n3 -=1
+        n2 = (num - 3*n3)//2
+
+        return pow(3,n3)*pow(2,n2)
+
+S = Solution()
 for i in range(4, 20):
-    print(Maxproduct(num = i))
-    print(maxproduct(num = i))
+    print(S.Maxproduct(num = i))
+    print(S.maxproduct(num = i))
