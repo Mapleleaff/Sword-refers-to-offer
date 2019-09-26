7347c48bec5689894f125dff890f1502fdfab7f2
Wu Tianji <wtj729651395@163.com>
2019-09-26 19:58:24 +0800

    Update 题16-数值的整数次方.py

--- 题16-数值的整数次方.py
+++ 题16-数值的整数次方.py
@@ -1,5 +1,15 @@
+"""
+题16-数值的整数次方
+给定一个 double 类型的浮点数 base 和 int 类型的整数 exponent。
+求 base 的 exponent 次方。
+"""
+
 class WithExponent:
     def Power(self, base, exponent):
+
+        if abs(base - 0) < 0.0001:
+            return None
+
         if exponent == 0:
             return 0
 
@@ -16,5 +26,7 @@ class WithExponent:
 
         return result
 
+    
 S = WithExponent()
+print(S.Power(0, -2))
 print(S.Power(2, -2))
