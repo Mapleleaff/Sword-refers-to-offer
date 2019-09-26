9590bce9333f5efe6ebaf60e921fa8af96d2703e
Wu Tianji <wtj729651395@163.com>
2019-09-26 18:51:32 +0800

    Update 题21-调整数组顺序使奇数位于偶数前面.py

--- 题21-调整数组顺序使奇数位于偶数前面.py
+++ 题21-调整数组顺序使奇数位于偶数前面.py
@@ -5,6 +5,17 @@
 """
 
 class Solution(object):
+    # 两列表解法
+    def orderList(self, array):
+        odd, even = [], []
+        for i in array:
+            if self.isEvein(i):
+                even.append(i)
+            else:
+                odd.append(i)
+        return odd + even
+
+    # 冒泡思想解法
     def orderArray(self, array):
         for i in range(len(array)-1, -1, -1):
             for j in range(i):
@@ -12,5 +23,34 @@ class Solution(object):
                     array[j], array[j+1] = array[j+1], array[j]
         return array
 
+    # 扩展性能解法
+    def ReorderOddEven(self, array):
+        length = len(array)
+        return self.Reorder(array,length, func = self.isEvein)
+
+    def Reorder(self, array, length, func):
+        if length == 0:
+            return
+        begin, end = 0, length -1
+        while begin < end:
+            while begin < end and not func(array[begin]):
+                begin +=1
+            while begin <end and func(array[end]):
+                end -= 1
+
+            if begin < end:
+                array[begin], array[end] = array[end], array[begin]
+        return array
+
+    def isEvein(self, n):
+        return not n & 1
+
+    def isNegtive(self, n):
+        return n >=0
+
+
 S = Solution()
-print(S.orderArray([1,2,3,4,5,6,7,8,9,10,11,12,13]))
+print(S.orderList([1,2,3,4,5,6,7,8,9,]))
+print(S.orderArray([1,2,3,4,5,6,7,8,9,]))
+print(S.ReorderOddEven([1,2,3,4,5,6,7,8,9]))
+print(S.ReorderOddEven([-1,2,-3,4,5,-6,-7]))
