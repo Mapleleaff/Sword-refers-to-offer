3d261527c06c20bbc4a9e60be62545e3a93ba4e5
Wu Tianji <wtj729651395@163.com>
2019-11-08 16:52:36 +0800

    Update 题39-数组中出现次数超过一半的数字.py

--- 题39-数组中出现次数超过一半的数字.py
+++ 题39-数组中出现次数超过一半的数字.py
@@ -22,7 +22,7 @@ class Solution:
         num = arr[0]
         count = 1
         for i in range(1, len(arr[1:])):
-            if arr[i] == arr[i-1]:
+            if arr[i] == num:
                 count += 1
             else:
                 count -= 1
